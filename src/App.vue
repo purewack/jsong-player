c95b46767eb24385887212c718a490fcf7451877
purewack <bobimaster15@gmail.com>
2024-01-05 18:54:03 +0000

    [dev] testing local package import

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 
+import JSONg from 'jsong-audio/dist'
+const player = new JSONg('all');
+player.parse('sample/audio.jsong').then(()=>{
+    player.play();
+})
+
 </script>
 
 <template>
-
+<nav>JSONg</nav>
+<!-- <button @click='player.play()'>Next</button> -->
 </template>
 
 <style>
